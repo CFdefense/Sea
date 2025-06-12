636cc0b055b040e470efd3436494c8a4669fba07
CFdefense <CFdefence@gmail.com>
2025-06-11 23:42:41 -0400

    contuining

--- src/lexer/token.go
+++ src/lexer/token.go
@@ -1,5 +1,14 @@
 package lexer
 
+// Based on my research I believe the best way to go about the lexer is as follows
+//
+// 1. Define allowed tokens using regex
+// 2. Convert Regex to NFA (Thompson's Algorithm)
+// 3. Compute ε-Closure of States
+// 4. Convert NFA to DFA 
+//
+//
+
 // 'Enum' for token types
 // using iota assigns successive integer values
 // ie: first defined token is 0, second is 1 etc
@@ -7,10 +16,17 @@ type TokenType int
 
 // token enum definitions
 const (
-	T_OPENING_BRACE TokenType = iota
-	T_CLOSING_BRACE
-	T_OPENING_PAREN
-	T_CLOSING_PAREN
+	T_OPENING_BRACE TokenType = iota	// '{'
+	T_CLOSING_BRACE						// '}'
+	T_OPENING_PAREN						// '('
+	T_CLOSING_PAREN						// ')'
+	T_OPENING_BRACK						// '['
+	T_CLOSING_BRACK						// ']'
+	T_OP_PLUS							// '+'
+	T_OP_DASH							// '
+	T_OP_SLASH
+	T_OP_
+	
 )
 
 // Individual token object
