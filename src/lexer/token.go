dd14e469b202d8bfb78257a61e0f49df160d7367
CFdefense <CFdefence@gmail.com>
2025-07-07 21:25:21 -0400

    regexes

--- src/lexer/token.go
+++ src/lexer/token.go
@@ -1,5 +1,41 @@
 package lexer
 
+import "regexp"
+
+// Regex patterns for token matching
+var (
+	// Patterns for each token type
+	IdentifierPattern = regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_]*`)
+
+	// Operators including all arithmetic, logical, bitwise, and assignment operators
+	OperatorPattern = regexp.MustCompile(`^(\+\+|--|==|!=|<=|>=|&&|\|\||->|<<|>>|//|[-+*/%<>!&|^~=]=|[-+*/%<>!&|^~])`)
+
+	// Constants (all uppercase)
+	ConstantPattern = regexp.MustCompile(`^[A-Z][A-Z0-9_]*`)
+
+	// All language keywords
+	KeywordPattern = regexp.MustCompile(`^(if|else|while|do|for|match|enum|struct|const|void|int|bool|mut|return|switch|case|default|break|continue|goto|sizeof|asm)\b`)
+
+	// Numeric literals including hex and binary
+	NumberPattern = regexp.MustCompile(`^(0x[0-9a-fA-F]+|0b[01]+|[0-9]+)`)
+
+	// Boolean literals
+	BoolPattern = regexp.MustCompile(`^(true|false)\b`)
+
+	// Punctuators including all delimiters and structural tokens
+	PunctuatorPattern = regexp.MustCompile(`^[{}\[\]();,.:?]`)
+
+	// Special tokens including decorators and reference operators
+	SpecialPattern = regexp.MustCompile(`^(@|&mut\b|&|#|\$)`)
+
+	// String literals with escape sequences
+	StringPattern = regexp.MustCompile(`^"([^"\\]|\\.)*"`)
+
+	// Comments
+	SingleLineCommentPattern = regexp.MustCompile(`^//[^\n]*`)
+	MultiLineCommentPattern  = regexp.MustCompile(`^/\*[\s\S]*?\*/`)
+)
+
 // 'Enum' for token types
 // using iota assigns successive integer values
 // ie: first defined token is 0, second is 1 etc
